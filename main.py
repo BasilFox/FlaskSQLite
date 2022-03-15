8e5f3c13b3aa6dbedc94c1af21bbd03ae91c1c04
BasilFox <vova1883.6g@ya.ru>
2022-03-15 16:41:35 +0300

    Модель формы авторизации пользователя

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_user
 
 from data import db_session
 from data.jobs import Jobs
 from data.users import User
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/mars_explorer.sqlite")
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,6 +31,8 @@ def index():
             leader = f'{user.name} {user.surname}'
             ekipash[job.team_leader] = leader
     return render_template('index.html', jobs=raboty, names=ekipash)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -49,8 +62,21 @@ def register():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
-@app.route('/login')
-def login ():
-    return render_template('base.html', title='Авторизация')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
